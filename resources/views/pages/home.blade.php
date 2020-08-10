bf2b2fa70271b5abd9a9ffde5c6fb01e3f13f93e
ryotinjpn <ryotinjpn@gmail.com>
2020-08-10 19:22:55 +0900

    add header icon

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,14 +3,13 @@
 @section('title', 'BOW')
 
 @section('content')
-<div class="center">
-  <div class="top_image">
-    <img class="top_dog" src="images/dogtop.jpg">
-    <div class="top_image2">
-      <img class="top_logo" src="images/logo.png">
-      <a href="＃" class="top_btn">新規登録</a>
+    <div class="center">
+        <div class="top_image">
+            <img class="top_dog" src="images/dogtop.jpg">
+            <div class="top_image2">
+                <img class="top_logo" src="images/logo.png">
+                <a href="＃" class="top_btn">新規登録</a>
+            </div>
+        </div>
     </div>
-  </div>
-  
-</div>
 @endsection
