53534c513b75d9b9b210d57d15acef5ba5b8b494
ryotinjpn <ryotinjpn@gmail.com>
2020-08-11 13:13:23 +0900

    fix register view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,8 +7,8 @@
             <form method="POST" action="{{ route('login') }}">
                 @csrf
                 <div class="form-item">
-                    <input id="email" type="email" class="form-control 
-                            @error('email') is-invalid @enderror" name="email" placeholder="Email Address"
+                    <input id="email" type="email" placeholder="Email Address" class="form-control 
+                            @error('email') is-invalid @enderror" name="email" 
                         value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                     @error('email')
@@ -18,8 +18,8 @@
                     @enderror
                 </div>
                 <div class="form-item">
-                    <input id="password" type="password" class="form-control" placeholder="Password" @error('password')
-                        is-invalid @enderror name="password" required autocomplete="current-password">
+                    <input id="password" type="password" placeholder="Password" class="form-control  @error('password')
+                        is-invalid @enderror" name="password" required autocomplete="current-password">
 
                     @error('password')
                     <span class="invalid-feedback" role="alert">
@@ -34,12 +34,12 @@
                 </div>
             </form>
             <div class="form-footer">
-                <p><a href="#">Create an account</a></p>
-                @if (Route::has('password.request'))
+                <p><a href="{{ route('register') }}">Create an account</a></p>
+               {{--  @if (Route::has('password.request'))
                     <a class="btn btn-link" href="{{ route('password.request') }}">
                         {{ __('Forgot Your Password?') }}
                     </a>
-                @endif
+                @endif --}}
             </div>
         </div>
     </div>
