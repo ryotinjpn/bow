0f1f667356ac504fda69612b03da2bc0f87db2cb
ryotinjpn <ryotinjpn@gmail.com>
2020-08-17 21:46:50 +0900

    index.blade.phpにユーザー詳細へのリンクを追加

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -5,18 +5,25 @@
 @section('content')
     <div class="container">
         <div class="main_view">
-
-            @foreach ($users as $user)
-                <div class="user_top">
-                    @if (empty($user->image))
-                        <img src="/images/usericon.png" class="icon_image_prof">
-                    @else
-                        <img src="{{ $user->image }}" class="icon_image_prof">
-                    @endif
-                    <span class="user_name">{{ $user->name }}<span>
-                </div>
-            @endforeach
-
+            <h1>ユーザー一覧</h1>
+            <div class="all_user">
+                <ul>
+                    @foreach ($users as $user)
+                        <li>
+                            <div class="users">
+                                @if (empty($user->image))
+                                    <a href="{{ action('UsersController@show', $user->id) }}"><img
+                                            src="/images/usericon.png" class="icon_image_feed"></a>
+                                @else
+                                    <a href="{{ action('UsersController@show', $user->id) }}"><img src="{{ $user->image }}"
+                                            class="icon_image_feed"></a>
+                                @endif
+                                <a href="{{ action('UsersController@show', $user->id) }}"
+                                    class="user_name">{{ $user->name }}</a>
+                            </div>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
         </div>
-    </div>
-@endsection
+    @endsection
