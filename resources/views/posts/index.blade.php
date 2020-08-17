3e459201c7903019dc618f1ff050166a91946c5e
ryotinjpn <ryotinjpn@gmail.com>
2020-08-17 15:12:36 +0900

    ユーザーの登録画像を表示させた

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -36,16 +36,24 @@
                     <ul>
                         @forelse ($posts as $post)
                             <li>
-                                <div>
-                                    <a href="{{ action('UsersController@show', $post->user_id) }}">{{ $post->user->name }}</a>
-                                    @if (File::extension($post->picture) == 'jpeg' || File::extension($post->picture) == 'jpg' || File::extension($post->picture) == 'png' || File::extension($post->picture) == 'gif')
-                                        <img src="{{ $post->picture }}" width="100%" height="100%">
+                                <div class="user_timeline">
+                                    @if (empty($post->user->image))
+                                        <a href="{{ action('UsersController@show', $post->user_id) }}"><img
+                                                src="/images/usericon.png" class="icon_image_feed"></a>
                                     @else
-                                        <video src="{{ $post->picture }}" width="100%" height="100%" controls="controls"></video>
+                                        <a href="{{ action('UsersController@show', $post->user_id) }}" ><img
+                                                src="{{ $post->user->image }}" class="icon_image_feed"></a>
                                     @endif
-                                    <div>{{ $post->content }}</div>
-                                    <div>{{ $post->created_at->diffForHumans() }}</div>
+                                    <a href="{{ action('UsersController@show', $post->user_id) }}"
+                                        class="user_name">{{ $post->user->name }}</a>
                                 </div>
+                                @if (File::extension($post->picture) == 'jpeg' || File::extension($post->picture) == 'jpg' || File::extension($post->picture) == 'png' || File::extension($post->picture) == 'gif')
+                                    <img src="{{ $post->picture }}" width="100%" height="100%">
+                                @else
+                                    <video src="{{ $post->picture }}" width="100%" height="100%" controls="controls"></video>
+                                @endif
+                                <div>{{ $post->content }}</div>
+                                <div>{{ $post->created_at->diffForHumans() }}</div>
                             </li>
                         @empty
                             <li>投稿はまだありません</li>
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -6,8 +6,12 @@
     <div class="container">
         <div class="main_view">
             <div class="user_top">
-                <img src="/images/usericon.png">
-                <span>{{ $user->name }}<span>
+                @if (empty($user->image))
+                    <img src="/images/usericon.png" class="icon_image_prof">
+                @else
+                    <img src="{{ $user->image }}" class="icon_image_prof">
+                @endif
+                <span class="user_name">{{ $user->name }}<span>
             </div>
             <div>{{ $user->profile }}</div>
             <a href="{{ $user->youtube }}">YouTubeチャンネル</a>
