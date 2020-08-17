f459cdd98ab7a8b290863ccb4b8c2deff9f5bb63
ryotinjpn <ryotinjpn@gmail.com>
2020-08-17 10:51:59 +0900

    ヘッダーにログイン中のユーザーページへのリンクを追加

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,8 +38,8 @@
                 @endauth
                 <ul class="nav navbar-nav navbar-right icons_bar_rigth">
                     @auth
-                        {{-- <li><a href="#"><i class="fas fa-user fa_icon"></i></a></li>
-                        <li><a href="#"><i class="fas fa-dog fa_icon"></i></a></li>
+                        <li><a href="{{ action('UsersController@show', Auth::user()->id) }}"><i class="fas fa-user fa_icon"></i></a></li>
+                        {{-- <li><a href="#"><i class="fas fa-dog fa_icon"></i></a></li>
                         <li><a href="#"><i class="fas fa-hands-helping fa_icon"></i></a></li> --}}
                         @unless ( Auth::user()->email == "guest@example.com" )
                         <li><a href="{{ url('/users/edit') }}"><i class="fas fa-cog fa_icon"></i></a></li>
@@ -63,65 +63,6 @@
     @yield('content')
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-    {{-- <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse"
-                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                    aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                        </li>
-                        @if (Route::has('register'))
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                            </li>
-                        @endif
-                        @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }} <span class="caret"></span>
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                                    style="display: none;">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        <main class="py-4">
-            @yield('content')
-        </main>
-    </div> --}}
 </body>
 
 </html>
