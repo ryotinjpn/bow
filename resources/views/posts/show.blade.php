ad1ca243f827daacad2e97b64755bd82603b1eb8
ryotinjpn <ryotinjpn@gmail.com>
2020-08-20 17:02:49 +0900

    posts/show.blade.phpの不要なコードを削除

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,10 +3,10 @@
 @section('title', 'BOW')
 
 @section('content')
-    <div class="home_view">
-        <div>
-            <ul>
-                <li>
+    <div class="container">
+        <div class="main_view">
+            <div class="home_view">
+                <div>
                     <div class="user_timeline">
                         @if (empty($post->user->image))
                             <a href="{{ action('UsersController@show', $post->user_id) }}"><img src="/images/usericon.png"
@@ -25,39 +25,9 @@
                     @endif
                     <div>{{ $post->content }}</div>
                     <div>{{ $post->created_at->diffForHumans() }}</div>
-                </li>
-            </ul>
+                </div>
+            </div>
         </div>
     </div>
-    {{-- <p>{!! nl2br(e($post->body)) !!}</p> --}}
-
-    {{-- <h2>Comments</h2>
-    <ul>
-        @forelse ($post->comments as $comment)
-        <li>
-            {{ $comment->body }}
-            <a href="#" class="del" data-id="{{ $comment->id }}">[x]</a>
-            <form method="post" action="{{ action('CommentsController@destroy', [$post, $comment]) }}"
-                id="form_{{ $comment->id }}">
-                {{ csrf_field() }}
-                {{ method_field('delete') }}
-            </form>
-        </li>
-        @empty
-        <li>No comments yet</li>
-        @endforelse
-    </ul>
-    <form method="post" action="{{ action('CommentsController@store', $post) }}">
-        {{ csrf_field() }}
-        <p>
-            <input type="text" name="body" placeholder="enter comment" value="{{ old('body') }}">
-            @if ($errors->has('body'))
-                <span class="error">{{ $errors->first('body') }}</span>
-            @endif
-        </p>
-        <p>
-            <input type="submit" value="Add Comment">
-        </p>
-    </form> --}}
     <script src="/js/main.js"></script>
 @endsection
