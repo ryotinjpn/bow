60bc26809f27a7402793875c598da86bee6f4b82
ryotinjpn <ryotinjpn@gmail.com>
2020-08-21 19:31:22 +0900

    CommentsControllerのstoreアクションを更新

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -6,10 +6,12 @@ use Illuminate\Http\Request;
 
 class CommentsController extends Controller
 {
-    public function store(PostRequest $request)
+    public function store(Request $request, Post $post)
     {
-        $comment = new Comment();
+        $comment = new Comment(['text'=> $request->text]);
+        $comment->user_id = Auth::user()->id;
+        $comment->post_id = $post->id;
         $comment->save();
-        return redirect('/');
+        return redirect()->action('PostsController@show', $post);
     }
 }
