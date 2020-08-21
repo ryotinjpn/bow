7928a82a5c3fe210d76151e49f3f42369b899b25
ryotinjpn <ryotinjpn@gmail.com>
2020-08-21 19:23:07 +0900

    commentsテーブルのマイグレーションファイルを作成

--- database/migrations/2020_08_21_190750_create_comments_table.php
+++ database/migrations/2020_08_21_190750_create_comments_table.php
@@ -15,6 +15,9 @@ class CreateCommentsTable extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->text('text');
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('post_id')->constrained('posts');
             $table->timestamps();
         });
     }
