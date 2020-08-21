92fc4a29969c3231a47d772293491cdf4d645f95
ryotinjpn <ryotinjpn@gmail.com>
2020-08-21 19:05:50 +0900

    CommentsController.phpにstoreアクションを追加

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class CommentsController extends Controller
 {
-    //
+    public function store(PostRequest $request)
+    {
+        $comment = new Comment();
+        $comment->save();
+        return redirect('/');
+    }
 }
