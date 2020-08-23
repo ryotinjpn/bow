40b77caedd207db1f9565f3fd81e4c8c9a42e6d9
ryotinjpn <ryotinjpn@gmail.com>
2020-08-23 20:58:36 +0900

    text', user_id, post_idの代入を許可

--- app/Comment.php
+++ app/Comment.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
+    protected $fillable = ['text','user_id','post_id'];
+
     public function user()
     {
         return $this->belongsTo('App\User');
