65f1e1238c4f18ed44252c74d03afc8193c808b0
ryotinjpn <ryotinjpn@gmail.com>
2020-08-28 21:34:59 +0900

    like_relationshipsのマイグレーションファイルを編集

--- database/migrations/2020_08_25_114726_create_like_relationships_table.php
+++ database/migrations/2020_08_25_114726_create_like_relationships_table.php
@@ -15,19 +15,9 @@ class CreateLikeRelationshipsTable extends Migration
     {
         Schema::create('like_relationships', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained('users');
-            $table->foreignId('post_id')->constrained('posts');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('post_id')->constrained('posts')->onDelete('cascade');
             $table->timestamps();
-
-            $table->foreign('user_id')
-                  ->references('id')
-                  ->on('users')
-                  ->onDelete('cascade');
-
-            $table->foreign('post_id')
-                  ->references('id')
-                  ->on('posts')
-                  ->onDelete('cascade'); 
         });
     }
 
