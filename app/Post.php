9ebe0da243aae3a54136fa6642228994a257fb3e
ryotinjpn <ryotinjpn@gmail.com>
2020-08-30 18:55:09 +0900

    PostモデルにLike_relationshipsモデルへのリレーションを追加

--- app/Post.php
+++ app/Post.php
@@ -3,6 +3,8 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Auth;
+use App\Like_relationship;
 
 class Post extends Model
 {
@@ -17,5 +19,15 @@ class Post extends Model
     public function user()
     {
         return $this->belongsTo('App\User');
+    }
+
+    public function Like_relationships()
+    {
+      return $this->hasMany('App\Like_relationship');
+    }
+
+    public function like_by()
+    {
+      return Like_relationship::where('user_id', Auth::user()->id)->first();
     }
 }
