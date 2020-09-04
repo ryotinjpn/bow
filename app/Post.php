47e99d3a0ce92c760524e17b14df67a41d9d3fc8
ryotinjpn <ryotinjpn@gmail.com>
2020-09-04 20:17:25 +0900

    ログイン中のユーザーのみいいねが押せるように設定

--- app/Post.php
+++ app/Post.php
@@ -21,13 +21,24 @@ class Post extends Model
         return $this->belongsTo('App\User');
     }
 
-    public function Likes()
-    {
-      return $this->hasMany('App\Like');
+    public function likes()
+  {
+    return $this->hasMany(Like::class, 'post_id');
+  }
+
+  public function is_liked_by_auth_user()
+  {
+    $id = Auth::id();
+
+    $likers = array();
+    foreach($this->likes as $like) {
+      array_push($likers, $like->user_id);
     }
 
-    public function like_by()
-    {
-      return Like::where('user_id', Auth::user()->id)->first();
+    if (in_array($id, $likers)) {
+      return true;
+    } else {
+      return false;
     }
+  }
 }
