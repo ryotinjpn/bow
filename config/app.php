3c152be26418a26c5ecb7dbffc6034eb79835b20
ryotinjpn <ryotinjpn@gmail.com>
2020-09-04 20:25:28 +0900

    CounterCacheをprovidersから削除

--- config/app.php
+++ config/app.php
@@ -175,8 +175,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
-        kanazaca\CounterCache\CounterCacheServiceProvider::class,
-
     ],
 
     /*
