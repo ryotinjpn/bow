a8c8151f5a4b3c42dc8291a22a1bb4b7ba63c5d3
松下諒多 <r.matsushita@nosh.jp>
2021-01-19 09:43:27 +0900

    コメントの保存処理を見直し

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -12,12 +12,11 @@ class CommentsController extends Controller
 {
     public function store(Request $request, Post $post)
     {
-        $comment = new Comment([
+        Comment::create([
             'text'    => $request->text,
             'user_id' => Auth::user()->id,
             'post_id' => $post->id,
         ]);
-        $comment->save();
 
         return redirect('posts/' . $post->id);
     }
